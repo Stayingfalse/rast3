9e2a0253fbeb484295d15ffe777f383ec89197ce
Stayingfalse <cezz@users.noreply.github.com>
2025-11-18 17:41:29 +0000

    fix: reorder imports in page.tsx for consistency and clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { AuthShowcase } from "~/app/_components/auth-showcase";
 import { KudosFeed } from "~/app/_components/kudos-feed";
 import { WishlistManager } from "~/app/_components/wishlist-manager";
 import { HydrateClient } from "~/trpc/server";
+import HideMarketingIfAuthenticated from "./_components/hide-marketing-if-authenticated";
 import HomeHeaderClientWrapper from "./_components/home-header-client-wrapper";
 import HomeMarketingServer from "./_components/home-marketing-server";
-import HideMarketingIfAuthenticated from "./_components/hide-marketing-if-authenticated";
 import KudosAreaClient from "./_components/kudos-area-client";
 
 export default async function Home() {
